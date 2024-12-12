5c85040e99154c49d98e336a5bbebe23af47662d
xAldox995 <valzanialdo@gmail.com>
2024-12-12 23:38:40 +0100

    add crypto price actions

---
+++ src/redux/actions/cryptoPriceActions.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { CRYPTO_COMPARE_PREZZO_FAILURE, CRYPTO_COMPARE_PREZZO_REQUEST, CRYPTO_COMPARE_PREZZO_SUCCESS } from "./types";
+import { API_URL } from "./authActions";
+
+export const fetchCryptoPrice = (symbol) => (dispatch) => {
+    dispatch({ type: CRYPTO_COMPARE_PREZZO_REQUEST, payload: symbol });
+    axios.get(`${API_URL}/crypto/price?symbol=ETH`, {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+        }
+    })
+        .then((response) => {
+            dispatch({ type: CRYPTO_COMPARE_PREZZO_SUCCESS, payload: response.data });
+        })
+        .catch((error) => {
+            dispatch({
+                type: CRYPTO_COMPARE_PREZZO_FAILURE,
+                payload: error.response?.data?.message || error.message,
+            });
+        });
+};
